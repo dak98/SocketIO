ba70b9c1000db87b455779d7971bf0b1391d6c93
Jakub Czajka <jakub.czajka1998@gmail.com>
2019-08-11 21:36:10 +0200

    Small bugfixes to the bind() implementation

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,4 +1,4 @@
-#include <communication.hpp>
+#include <connection.hpp>
 
 #include <stdexcept>
 #include <sys/socket.h>
@@ -16,20 +16,20 @@ static inline auto bind(socket const& unbound, sockaddr const* addr) -> void
 		                 get_errno_as_string()};
 }
     
-auto bind(socket const& unbound, ip_socket_address const& to_bind) -> void
+auto bind(socket const& unbound, ipv4_socket_address const& to_bind) -> void
 {
-    if (unbound.get_ip_version() == ip_protocol::IPv6)
+    if (unbound.get_ip_protocol() == ip_protocol::IPv6)
         throw std::logic_error{"Cannot bind an IPv4 address to a IPv6 socket"};
     sockaddr_in addr = to_bind.get_native_handle();
-    socket_io::bind(*this, reinterpret_cast<sockaddr const*>(&addr));
+    socket_io::bind(unbound, reinterpret_cast<sockaddr const*>(&addr));
 }
 
 auto bind(socket const& unbound, ipv6_socket_address const& to_bind) -> void
 {
-    if (unbound.get_ip_version() == ip_protocol::IPv4)
+    if (unbound.get_ip_protocol() == ip_protocol::IPv4)
 	throw std::logic_error{"Cannot bind an IPv6 address to a IPv4 socket"};
     sockaddr_in6 addr = to_bind.get_native_handle();
-    socket_io::bind(*this, reinterpret_cast<sockaddr const*>(&addr));
+    socket_io::bind(unbound, reinterpret_cast<sockaddr const*>(&addr));
 }
     
 } // socket_io
