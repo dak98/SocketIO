eba30214b54f3eedd8b3b4c2ac5ed979cfb3a51b
Jakub Czajka <jakub.czajka1998@gmail.com>
2019-08-13 21:47:36 +0200

    Updated the epoll class

--- include/epoll.hpp
+++ include/epoll.hpp
@@ -2,32 +2,60 @@
 #define SOCKETIO_EPOLL_HPP_
 
 #include <cstdint>
-#include <sys/epoll.h>
+#include <vector>
+#include <utility>
+
+#include "socket.hpp"
 
 namespace socket_io
 {
 
 class epoll
 {
-private:
-    int epfd;
+    using socket_data = std::pair<int, ip_protocol>;
 public:
-    epoll();
-    ~epoll();
-        
-    /*
-     * Copy operations are deleted because both file descriptors would refer to
-     * the same epoll object
-     */       
-    epoll(epoll const& epoll) = delete;
-    epoll& operator=(epoll const& epoll) = delete;
-
-    epoll(epoll&& epoll) noexcept;
-    epoll& operator=(epoll&& epoll) noexcept;            
+    /* 
+     * Because of this struct we hide the usage of union and the undefined
+     * behaviour that comes with it
+     */
+    struct event
+    {
+	uint32_t flags;
+	socket caused_by;
+    }; // event
     
-    void add(int const fd, uint32_t const events) const;
-    void remove(int const fd) const;
-    epoll_event get_event() const;
+    /*
+     * @throws - std::runtime_error => an error occured while creating epoll
+     */
+    epoll();
+        
+    // Copy operations are deleted as they would splot the objects
+    epoll(epoll const& other) = delete;
+    epoll(epoll&& other) noexcept;
+
+    ~epoll() noexcept;
+    
+    auto operator=(epoll const& other) -> epoll& = delete;
+    auto operator=(epoll&& other) noexcept -> epoll&;    
+    
+    /*
+     * @throws - std::runtime_error => an error occured while adding a fd
+     */
+    auto add(socket const& to_monitor, uint32_t const events) -> void;
+    /*
+     * @throws - std::runtime_error => an error occured while removing a fd
+     */
+    auto remove(socket const& to_stop_monitor) -> void;
+
+    auto get_native_handle() const noexcept -> int { return handle; }
+
+    /*
+     * @throws - std::runtime_error => an error occured while acquiring an event
+     */
+    auto get_event() const -> event;
+private:
+    int handle;
+    std::vector<socket_data> monitored_sockets;
 }; // epoll
     
 } // socket_io
---
+++ src/epoll.cpp
@@ -0,0 +1,83 @@
+#include <epoll.hpp>
+
+#include <algorithm>
+#include <sys/epoll.h>
+#include <stdexcept>
+#include <unistd.h>
+
+#include <utils.hpp>
+
+#define EPFD_AFTER_MOVE 1
+
+namespace socket_io
+{
+
+epoll::epoll(){
+    handle = epoll_create1(0);
+    if (handle == -1)
+	throw std::runtime_error{"An error occured while creating epoll: " +
+		                 get_errno_as_string()};
+}
+
+epoll::epoll(epoll&& other) noexcept
+    : handle{other.handle}
+{
+    other.handle = EPFD_AFTER_MOVE;
+}
+
+epoll::~epoll() noexcept
+{
+    // Target of a move operation is responsible for closing the descriptor        
+    if (handle != EPFD_AFTER_MOVE)
+	close(handle);
+}
+
+auto epoll::operator=(epoll&& other) noexcept -> epoll&
+{
+    if (this == &other)
+	return *this;
+    
+    handle = other.handle;
+    other.handle = EPFD_AFTER_MOVE;
+    return *this;
+}
+
+auto epoll::add(socket const& to_monitor, uint32_t const events) -> void
+{
+    int const sockfd = to_monitor.get_native_handle();
+    epoll_event event { events, { .fd = sockfd } };
+    if (epoll_ctl(handle, EPOLL_CTL_ADD, sockfd, &event) == -1)
+	throw std::runtime_error{"An error occured while adding a fd: " +
+		                 get_errno_as_string()};
+    monitored_sockets.push_back({sockfd, to_monitor.get_ip_protocol()});
+}
+
+auto epoll::remove(socket const& to_stop_monitor) -> void
+{
+    int const sockfd = to_stop_monitor.get_native_handle();
+    epoll_event event { 0, { .fd = sockfd } };
+    if (epoll_ctl(handle, EPOLL_CTL_DEL, sockfd, &event) == -1)
+	throw std::runtime_error{"An error occured while removing a fd: " +
+		                 get_errno_as_string()};
+    auto new_end = std::remove_if(std::begin(monitored_sockets),
+				  std::end(monitored_sockets),
+				  [=](socket_data const& data)
+				  { return data.first == sockfd; });
+    monitored_sockets.erase(new_end, std::end(monitored_sockets));		   
+}
+
+auto epoll::get_event() const -> event
+{
+    epoll_event events[1];
+    if (epoll_wait(handle, events, 1, -1) == -1)
+	throw std::runtime_error{"An error occured while acquiring an event: " +
+		                 get_errno_as_string()};
+
+    auto it = std::find_if(std::begin(monitored_sockets),
+			   std::end(monitored_sockets),
+			   [=](socket_data const& data)
+			   { return data.first == events[0].data.fd; });
+    return {events[0].events, socket{it->first, it->second}};
+}
+    
+} // socket_io
