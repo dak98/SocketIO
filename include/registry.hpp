5e014ffd445721615fd000fef3c0af188219c122
Jakub Czajka <jakub.czajka1998@gmail.com>
2019-08-13 20:01:43 +0200

    Changed the data way sockets are stored in the registry_for_clients

--- include/registry.hpp
+++ include/registry.hpp
@@ -1,17 +1,30 @@
 #ifndef SOCKETIO_REGISTRY_HPP_
 #define SOCKETIO_REGISTRY_HPP_
 
+#include <boost/functional/hash.hpp>
+
+#include <functional>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
+#include "socket.hpp"
+    
 namespace socket_io
 {
 
-// It is NOT thread-safe.
+/*
+ * Holds a bimap between:
+ * - ID of the client given by the server,
+ * - a socket assigned to the server for communication with the client.
+ *
+ * It is NOT thread-safe
+ */
 class registry_of_clients
 {
     using client_id = int;
-    using socket_id = int;
+    using socket_for_client = std::pair<int, ip_protocol>;
+    using hash = boost::hash<socket_for_client>;
 public:
     explicit registry_of_clients(int const base_id = 100)
 	: base_id{base_id} {}
@@ -20,28 +33,29 @@ public:
      * @returns Id of the new entry
      * @throws - std::runtime_error => no free ID available
      */
-    auto add(int const sockfd) -> int;
+    auto add(socket const& for_client) -> int;
 
     /*
      * @throws - std::out_of_range => element with specified key does not exist
      */
-    auto get_sockfd(int const id) const -> int
-    { return client_to_socket.at(id); }
-    auto get_id(int const sockfd) const -> int
-    { return socket_to_client.at(sockfd); }
+    auto get_socket(int const id) const -> socket;
+    auto get_id(socket const& for_client) const -> int;
 
-    auto remove_by_sockfd(int const sockfd) noexcept -> void;
-    auto remove_by_id(int const id) noexcept -> void;
+    /*
+     * @throws - std::out_of_range => element with specified key does not exist
+     */    
+    auto remove(socket const& for_client) -> void;
+    auto remove(int const id) -> void;
 
     /*
      * @throws - std::bad_alloc => could not allocate the vector
      */
     auto get_ids() const -> std::vector<client_id>;
-    auto get_sockfds() const -> std::vector<socket_id>;
+    auto get_sockets() const -> std::vector<socket>;
 private:
-    int const base_id;    
-    std::unordered_map<client_id, socket_id> client_to_socket;
-    std::unordered_map<socket_id, client_id> socket_to_client;
+    int const base_id;
+    std::unordered_map<client_id, socket_for_client> client_to_socket;
+    std::unordered_map<socket_for_client, client_id, hash> socket_to_client;
 }; // registry_of_clients
     
 } // socket_io
--- src/registry.cpp
+++ src/registry.cpp
@@ -6,15 +6,17 @@
 namespace socket_io
 {
 
-auto registry_of_clients::add(int const sockfd) -> int
+auto registry_of_clients::add(socket const& socket) -> int
 {
     int id = base_id;
     bool found_id = false;
     for (; id < std::numeric_limits<int>::max() && !found_id; id++)
 	if (client_to_socket.find(id) == client_to_socket.end())
 	{
-	    client_to_socket.emplace(id, sockfd);
-	    socket_to_client.emplace(sockfd, id);
+	    socket_for_client const sock{socket.get_native_handle(),
+		                         socket.get_ip_protocol()};
+	    client_to_socket.emplace(id, sock);
+	    socket_to_client.emplace(sock, id);
 	    found_id = true;
 	}
 
@@ -23,34 +25,49 @@ auto registry_of_clients::add(int const sockfd) -> int
     return id;
 }
 
-auto registry_of_clients::remove_by_sockfd(int const sockfd) noexcept -> void
+auto registry_of_clients::get_socket(int const id) const -> socket
 {
-    client_to_socket.erase(get_id(sockfd));
-    socket_to_client.erase(sockfd);
+    socket_for_client const sock = client_to_socket.at(id);
+    return socket{sock.first, sock.second};
 }
 
-auto registry_of_clients::remove_by_id(int const id) noexcept -> void
+auto registry_of_clients::get_id(socket const& socket) const -> int
 {
+    socket_for_client const sock{socket.get_native_handle(),
+	                         socket.get_ip_protocol()};
+    return socket_to_client.at(sock);
+}
+
+auto registry_of_clients::remove(socket const& socket) -> void
+{
+    int const id = get_id(socket);
+    socket_for_client const sock = client_to_socket.at(id);
     client_to_socket.erase(id);
-    socket_to_client.erase(get_sockfd(id));
+    socket_to_client.erase(sock);
+}
+
+auto registry_of_clients::remove(int const id) -> void
+{
+    socket_for_client const sock = client_to_socket.at(id);
+    client_to_socket.erase(id);
+    socket_to_client.erase(sock);
 }
 
 auto registry_of_clients::get_ids() const -> std::vector<int>
 {
     std::vector<int> ids(client_to_socket.size());
     int index = 0;
-    for (auto const& [id, sockfd] : client_to_socket)
+    for (auto const& [id, sock] : client_to_socket)
 	ids[index++] = id;
     return ids;
 }
 
-auto registry_of_clients::get_sockfds() const -> std::vector<int>
+auto registry_of_clients::get_sockets() const -> std::vector<socket>
 {
-    std::vector<int> sockfds(socket_to_client.size());
-    int index = 0;
-    for (auto const& [sockfd, id] : socket_to_client)
-	sockfds[index++] = sockfd;
-    return sockfds;
+    std::vector<socket> sockets;
+    for (auto const& [sock, id] : socket_to_client)
+	sockets.emplace_back(socket{sock.first, sock.second});
+    return sockets;
 }    
     
 } // socket_io
